1c84b5f6263f6c4498df4fc9e0bae3f6df5cf22b
Tomek Wytrebowicz <tomalecpub@gmail.com>
2015-10-04 18:11:34 +0200

    Add grunt-bump

--- gruntfile.js
+++ gruntfile.js
@@ -23,6 +23,23 @@ module.exports = function(grunt) {
         ]
       }
     },
-  });    
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json'],
+        commit: true,
+        commitMessage: '%VERSION%',
+        commitFiles: ['package.json', 'bower.json'],
+        createTag: true,
+        tagName: '%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: false,
+        // pushTo: 'origin',
+        globalReplace: false,
+        prereleaseName: false,
+        regExp: false
+      }
+    }
+  });
 grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-bump');
 };
